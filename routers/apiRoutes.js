fbb849821853654f864123989e553cb4c4fb4dd5
YeYint3424 <xino3424@gmail.com>
2023-11-06 12:00:12 +0630

    fix routes

--- routers/apiRoutes.js
+++ routers/apiRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const pileController = require('../controllers/pileController')
 
 router.get('/all-questions', pileController.allQuestions)
-router.get('/:id', pileController.ansForSingle)
-
 router.get('/question/:QuestionId', pileController.GetQuestionById)
 router.get('/:QuestionId/:AnswerId', pileController.Answer)
+router.get('/:id', pileController.ansForSingle)
+
 
 module.exports = router
